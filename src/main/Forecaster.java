301d0c6d22efc89ed317305cac76d4dff174e16b
ashishthukral <athukral7@gmail.com>
2015-12-19 14:23:27 -0800

    checking in code files.

---
+++ src/main/Forecaster.java
@@ -0,0 +1,111 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import util.HelperUtil;
+
+public class Forecaster {
+
+	private static final double INFLATION_PERCENTAGE = 3.5;
+
+	private static final int NO_OF_YEARS = 20;
+
+	private static final int NO_OF_ITERATIONS = 10_000;
+
+	private static final double BASE_AMOUNT = 100_000;
+
+	// 128,347 ,,, 197,565
+
+	// private static final double MEAN_CONSERVATIVE = 7.4;
+	// private static final double SD_CONSERVATIVE = 8.0;
+
+	// Conservative Return
+	private static final double MEAN_CONSERVATIVE = 6.189;
+
+	// Conservative Risk
+	private static final double SD_CONSERVATIVE = 6.3438;
+
+	// 104,320 ,,, 246,553
+
+	// Aggressive Return
+	private static final double MEAN_AGGRESSIVE = 9.4324;
+
+	// Aggressive Risk
+	private static final double SD_AGGRESSIVE = 15.675;
+
+	private final List<Double> results = new ArrayList<>(NO_OF_ITERATIONS);
+
+	public static void main(String[] args) {
+
+		Forecaster forecaster = new Forecaster();
+
+		HelperUtil.print("*** Calculating Conservative Profile ***     Mean={0} , Standard Deviation={1}", MEAN_CONSERVATIVE, SD_CONSERVATIVE);
+		forecaster.calculateProjections(MEAN_CONSERVATIVE, SD_CONSERVATIVE);
+
+		HelperUtil.print("\n*** Calculating Aggressive Profile ***     Mean={0} , Standard Deviation={1}", MEAN_AGGRESSIVE, SD_AGGRESSIVE);
+		forecaster.calculateProjections(MEAN_AGGRESSIVE, SD_AGGRESSIVE);
+	}
+
+	/**
+	 * Run iterations for projecting returns. Prints the output at end.
+	 * 
+	 * @param mean
+	 * @param sd
+	 */
+	public void calculateProjections(final double mean, final double sd) {
+
+		final long startTime = System.currentTimeMillis();
+
+		/*
+		 * NOTE - Using serial operations here as earlier using ExecutorService lead to higher running times probably due to more overheard
+		 * of maintaining threads than actual effort of projecting returns.
+		 * For thread pool size of even 100, time taken was around 5 times more than a simple for loop.
+		 */
+		for (int i = 0; i < NO_OF_ITERATIONS; i++) {
+			results.add(project(mean, sd));
+		}
+		// Sort to get percentiles, median. O(n log n) complexity.
+		Collections.sort(results);
+
+		final Double percentile10 = results.get(HelperUtil.calculatePercentileIndex(10, results.size()));
+		final double median = HelperUtil.calculateMedian(results);
+		final Double percentile90 = results.get(HelperUtil.calculatePercentileIndex(90, results.size()));
+
+		HelperUtil.print("10% percentile={0}", HelperUtil.round(percentile10));
+		HelperUtil.print("Median={0}", HelperUtil.round(median));
+		HelperUtil.print("90% percentile={0}", HelperUtil.round(percentile90));
+
+		System.out.println("\nAfter Adjusting Inflation\n");
+
+		HelperUtil.print("10% percentile={0}", HelperUtil.round(HelperUtil.adjustBackwardInflation(INFLATION_PERCENTAGE, NO_OF_YEARS, percentile10)));
+		HelperUtil.print("Median={0}", HelperUtil.round(HelperUtil.adjustBackwardInflation(INFLATION_PERCENTAGE, NO_OF_YEARS, median)));
+		HelperUtil.print("90% percentile={0}", HelperUtil.round(HelperUtil.adjustBackwardInflation(INFLATION_PERCENTAGE, NO_OF_YEARS, percentile90)));
+
+		long endTime = System.currentTimeMillis();
+		HelperUtil.print("Took {0} ms", endTime - startTime);
+		results.clear();
+	}
+
+	/**
+	 * Project for {@link #NO_OF_YEARS} starting from the {@link #BASE_AMOUNT}. Adjusts mean every year by a random SD value and returns the
+	 * Expected Return at end of term.
+	 * 
+	 * @param mean
+	 * @param sd
+	 * @return double
+	 */
+	private double project(final double mean, final double sd) {
+
+		double amount = BASE_AMOUNT;
+
+		for (byte i = 0; i < NO_OF_YEARS; i++) {
+			final double randomSD = HelperUtil.generateRandomSD(sd);
+			final double randomMean = mean + randomSD;
+			amount *= ((100 + randomMean) / 100);
+		}
+		return amount;
+	}
+
+}
---
+++ src/util/HelperUtil.java
@@ -0,0 +1,105 @@
+package util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Random;
+
+public class HelperUtil {
+
+	private static final Random RANDOM = new Random();
+	
+	/**
+	 * Generates a random SD value using the 68-95-99.7 rule. The value can be positive or negative.
+	 * @param oneSD
+	 * @return
+	 */
+	public static double generateRandomSD(final double oneSD) {
+
+		final double sigmaVar = RANDOM.nextDouble();
+		final double sd;
+
+		if (sigmaVar <= 0.6827) {
+			sd = RANDOM.nextDouble() * oneSD;
+		} else if (sigmaVar <= 0.9545) {
+			sd = 2 * RANDOM.nextDouble() * oneSD;
+		} else if (sigmaVar <= 0.9973) {
+			sd = 3 * RANDOM.nextDouble() * oneSD;
+		} else {
+			// for anything more than 3 sigma, treat as 4
+			sd = 4 * RANDOM.nextDouble() * oneSD;
+		}
+
+		final boolean isPositive = RANDOM.nextBoolean();
+		final double randomSD = isPositive ? sd : sd * -1;
+		return randomSD;
+	}
+
+	/**
+	 * Calculate inflation adjusted for what the passed amount would be noOfYears ago.
+	 * @param inflationPercentage
+	 * @param noOfYears
+	 * @param amount
+	 * @return double
+	 */
+	public static double adjustBackwardInflation(final double inflationPercentage, final int noOfYears, final double amount) {
+
+		final double interimDivisor = Math.pow((100 + inflationPercentage) / 100, noOfYears);
+		final double inflationAdjustedAmount = amount / interimDivisor;
+		return inflationAdjustedAmount;
+	}
+
+	/**
+	 * Rounds double amount using Banker's rounding method.
+	 * @param amount
+	 * @return double
+	 */
+	public static double round(final double amount) {
+
+		return new BigDecimal(amount).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+	}
+
+	/**
+	 * Returns median from the list based on even/odd size of list.
+	 * @param list
+	 * @return double
+	 */
+	public static double calculateMedian(final List<Double> list) {
+
+		final int size = list.size();
+		final double median;
+		if (size % 2 == 0) {
+			final int almostMiddleIndex = size / 2;
+			median = (list.get(almostMiddleIndex) + list.get(almostMiddleIndex - 1)) / 2;
+		} else {
+			final int middleIndex = size / 2;
+			median = list.get(middleIndex);
+		}
+		return median;
+	}
+
+	/**
+	 * Calculates index for a list for a particular percentile. NOTE - This is a simple implementation supporting sizes of multiple of 100s.
+	 * @param percentile
+	 * @param size
+	 * @return int
+	 */
+	public static int calculatePercentileIndex(final int percentile, final int size) {
+
+		return (size * percentile) / 100;
+	}
+
+	/**
+	 * Pretty prints messages using {@link MessageFormat#format(String, Object...)}. Written for brevity.
+	 * @param message
+	 * @param arguments
+	 */
+	public static void print(final String message, final Object... arguments) {
+
+		System.out.println(MessageFormat.format(message, arguments));
+	}
+
+
+
+}
